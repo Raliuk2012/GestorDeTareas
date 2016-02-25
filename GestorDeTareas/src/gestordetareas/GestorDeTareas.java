20fd0d44022adbd875c8e3777e78b9d25577fd42
raliuk <raliuk@raliuk-PC>
2016-02-26 00:53:23 +0100

    Actualizar

--- GestorDeTareas/src/gestordetareas/GestorDeTareas.java
+++ GestorDeTareas/src/gestordetareas/GestorDeTareas.java
@@ -16,6 +16,7 @@ public class GestorDeTareas {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Prueba de calendario");
     }
     
 }
